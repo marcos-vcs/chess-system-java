c4ca1a56252fca10764f5697ae1c0b9da579a55c
marcos <marcosviniciocardoso123@gmail.com>
2021-12-29 19:13:29 -0300

    this commit start implementation board and peaces

---
+++ chess-system/src/boardGame/Piece.java
@@ -0,0 +1,19 @@
+package boardGame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		super();
+		this.board = board;
+		this.position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+	
+	
+}
